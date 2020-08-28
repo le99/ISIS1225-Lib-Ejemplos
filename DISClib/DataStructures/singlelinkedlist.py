a0688619d949e2ead1272899e44148e53d0f146b
Dario Correal <dcorreal@uniandes.edu.co>
2020-08-28 17:28:52 -0500

    Maps y hash tables

--- DISClib/DataStructures/singlelinkedlist.py
+++ DISClib/DataStructures/singlelinkedlist.py
@@ -1,6 +1,7 @@
 """
- * Copyright 2020, Departamento de sistemas y Computación, Universidad de Los Andes
- * 
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
  *
  * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
  *
@@ -18,73 +19,76 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  """
- 
+
 import config
-from DISClib.DataStructures import listnode as node 
+from DISClib.DataStructures import listnode as node
 from DISClib.Utils import error as error
+assert config
 
 """
-  Este módulo implementa una estructura de datos lineal mediante una lista encadenada sencillamente 
-  para almacenar una colección de elementos.  Los elementos se cuentan desde la posición 1.
+  Este módulo implementa una estructura de datos lineal mediante una lista
+  encadenada sencillamente para almacenar una colección de elementos.
+  Los elementos se cuentan desde la posición 1.
 """
 
 
-def newList (cmpfunction=None):
+def newList(cmpfunction=None):
     """Crea una lista vacia.
 
-    Se inicializan los apuntadores a la primera y ultima posicion en None.  El tipo de la lista
-    se inicializa como SINGLE_LINKED
+    Se inicializan los apuntadores a la primera y ultima posicion en None.
+    El tipo de la listase inicializa como SINGLE_LINKED
     Args:
         cmpfunction: Función de comparación para los elementos de la lista
     Returns:
-        Un diccionario que representa la estructura de datos de una lista encadanada vacia.
+        Un diccionario que representa la estructura de datos de una lista
+        encadanada vacia.
 
     Raises:
 
     """
-    new_list = {'first':None, 'last':None, 'size':0, 'type':'SINGLE_LINKED', 'cmpfunction':cmpfunction}
+    new_list = {'first': None,
+                'last': None,
+                'size': 0,
+                'type': 'SINGLE_LINKED',
+                'cmpfunction': cmpfunction}
     return new_list
-
-
 
 
 def addFirst(lst, element):
     """Agrega un elemento a la lista en la primera posicion.
 
-    Agrega un elemento en la primera posición de la lista, ajusta el apuntador al primer elemento e incrementa 
-    el tamaño de la lista.
+    Agrega un elemento en la primera posición de la lista, ajusta el apuntador
+    al primer elemento e incrementa el tamaño de la lista.
 
     Args:
         lst:  La lista don de inserta el elemento
         element:  El elemento a insertar en la lista
 
     Returns:
-        La lista con el nuevo elemento en la primera posición, si el proceso fue exitoso  
+        La lista con el nuevo elemento en la primera posición, si el proceso
+        fue exitoso
 
     Raises:
         Exception
     """
     try:
-        new_node = node.newSingleNode (element)
-        new_node ['next'] = lst['first']
+        new_node = node.newSingleNode(element)
+        new_node['next'] = lst['first']
         lst['first'] = new_node
         if (lst['size'] == 0):
-            lst ['last'] = lst['first']
+            lst['last'] = lst['first']
         lst['size'] += 1
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->addFirst: ')
-       
-
-
+        error.reraise(exp, 'singlelinkedlist->addFirst: ')
 
 
 def addLast(lst, element):
     """ Agrega un elemento en la última posición de la lista.
 
-    Se adiciona un elemento en la última posición de la lista y se actualiza el apuntador a la útima posición. 
+    Se adiciona un elemento en la última posición de la lista y se actualiza
+     el apuntador a la útima posición.
     Se incrementa el tamaño de la lista en 1
-    
     Args:
         lst: La lista en la que se inserta el elemento
         element: El elemento a insertar
@@ -92,27 +96,23 @@ def addLast(lst, element):
     Raises:
         Exception
     """
-    try: 
-        new_node = node.newSingleNode (element)
+    try:
+        new_node = node.newSingleNode(element)
 
         if lst['size'] == 0:
             lst['first'] = new_node
         else:
             lst['last']['next'] = new_node
-        lst['last']= new_node
+        lst['last'] = new_node
         lst['size'] += 1
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->addLast: ')
+        error.reraise(exp, 'singlelinkedlist->addLast: ')
 
 
-
-
-
-def isEmpty (lst):
+def isEmpty(lst):
     """ Indica si la lista está vacía
-
-    Args: 
+    Args:
         lst: La lista a examinar
 
     Raises:
@@ -120,47 +120,44 @@ def isEmpty (lst):
     """
     try:
         return lst['size'] == 0
-    except Exception as exp: 
-        error.reraise (exp, 'singlelinkedlist->isEmpty: ')
-
-
+    except Exception as exp:
+        error.reraise(exp, 'singlelinkedlist->isEmpty: ')
 
 
 def size(lst):
     """ Informa el número de elementos de la lista.
-
     Args
-        lst: La lista a examinar
-    
-    Raises: 
-        Exception
-    """
-    try:
-        return lst['size'] 
-    except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->size: ')
-
-
-
-def firstElement (lst):
-    """ Retorna el primer elemento de una lista no vacía. No se elimina el elemento.
-
-    Args: 
         lst: La lista a examinar
 
     Raises:
         Exception
     """
     try:
-        if 'info' in lst['first']: 
+        return lst['size']
+    except Exception as exp:
+        error.reraise(exp, 'singlelinkedlist->size: ')
+
+
+def firstElement(lst):
+    """ Retorna el primer elemento de una lista no vacía.
+     No se elimina el elemento.
+
+    Args:
+        lst: La lista a examinar
+
+    Raises:
+        Exception
+    """
+    try:
+        if 'info' in lst['first']:
             return lst['first']['info']
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->fisrtElement: ')
+        error.reraise(exp, 'singlelinkedlist->fisrtElement: ')
 
 
-
-def lastElement (lst):
-    """ Retorna el último elemento de una  lista no vacia.  No se elimina el elemento.
+def lastElement(lst):
+    """ Retorna el último elemento de una  lista no vacia.
+        No se elimina el elemento.
 
     Args:
         lst: La lista a examinar
@@ -172,16 +169,16 @@ def lastElement (lst):
         if 'info' in lst['last']:
             return lst['last']['info']
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->lastElement: ')
+        error.reraise(exp, 'singlelinkedlist->lastElement: ')
 
 
-
-
-def getElement (lst, pos):
+def getElement(lst, pos):
     """ Retorna el elemento en la posición pos de la lista.
 
-    Se recorre la lista hasta el elemento pos, el cual  debe ser mayor que cero y menor o igual al tamaño de la lista.
-    Se retorna el elemento en dicha posición sin eleminarlo.  La lista no puede ser vacia.
+    Se recorre la lista hasta el elemento pos, el cual  debe ser
+    mayor que cero y menor o igual al tamaño de la lista.
+    Se retorna el elemento en dicha posición sin eleminarlo.
+    La lista no puede ser vacia.
 
     Args:
         lst: La lista a examinar
@@ -194,20 +191,20 @@ def getElement (lst, pos):
         searchpos = 1
         node = lst['first']
         while searchpos < pos:
-            searchpos+=1
+            searchpos += 1
             node = node['next']
         return node['info']
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->getElement: ')
+        error.reraise(exp, 'singlelinkedlist->getElement: ')
 
 
-
-
-def deleteElement (lst, pos):
+def deleteElement(lst, pos):
     """ Elimina el elemento en la posición pos de la lista.
-    
-    Elimina el elemento que se encuentra en la posición pos de la lista.  Pos debe ser mayor que cero y menor 
-    o igual al tamaño de la lista.   Se decrementa en un uno el tamñao de la lista. La lista no puede estar vacia.
+
+    Elimina el elemento que se encuentra en la posición pos de la lista.
+    Pos debe ser mayor que cero y menor o igual al tamaño de la lista.
+    Se decrementa en un uno el tamñao de la lista.
+    La lista no puede estar vacia.
 
     Args:
         lst: La lista a retoranr
@@ -224,24 +221,21 @@ def deleteElement (lst, pos):
             lst['first'] = lst['first']['next']
         elif(pos > 1):
             while searchpos < pos:
-                searchpos+=1
+                searchpos += 1
                 prev = node
                 node = node['next']
             prev['next'] = node['next']
         lst['size'] -= 1
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->deleteElement: ')
+        error.reraise(exp, 'singlelinkedlist->deleteElement: ')
 
 
-
-
-def removeFirst (lst):
-    """ Remueve el primer elemento de la lista. 
-    
-    Elimina y retorna el primer elemento de la lista.  El tamaño de la lista se decrementa en uno.  Si la lista
+def removeFirst(lst):
+    """ Remueve el primer elemento de la lista.
+    Elimina y retorna el primer elemento de la lista.
+    El tamaño de la lista se decrementa en uno.  Si la lista
     es vacía se retorna None.
-
     Args:
         lst: La lista a examinar
 
@@ -249,9 +243,9 @@ def removeFirst (lst):
         Exception
     """
     try:
-        if lst['first'] != None:
+        if lst['first'] is not None:
             temp = lst['first']['next']
-            node = lst['first'] 
+            node = lst['first']
             lst['first'] = temp
             lst['size'] -= 1
             if (lst['size'] == 0):
@@ -260,16 +254,14 @@ def removeFirst (lst):
         else:
             return None
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->removeFirst: ')
+        error.reraise(exp, 'singlelinkedlist->removeFirst: ')
 
 
-
-
-
-def removeLast (lst):
+def removeLast(lst):
     """ Remueve el último elemento de la lista.
-    
-    Elimina el último elemento de la lista  y lo retorna en caso de existir. El tamaño de la lista se decrementa en 1. 
+
+    Elimina el último elemento de la lista  y lo retorna en caso de existir.
+    El tamaño de la lista se decrementa en 1.
     Si la lista es vacía  retorna None.
 
     Args:
@@ -281,11 +273,11 @@ def removeLast (lst):
     try:
         if lst['size'] > 0:
             if lst['first'] == lst['last']:
-                node = lst['first'] 
+                node = lst['first']
                 lst['last'] = None
                 lst['first'] = None
             else:
-                temp = lst['first']    
+                temp = lst['first']
                 while temp['next'] != lst['last']:
                     temp = temp['next']
                 node = lst['last']
@@ -296,34 +288,33 @@ def removeLast (lst):
         else:
             return None
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->remoLast: ')
+        error.reraise(exp, 'singlelinkedlist->remoLast: ')
 
 
+def insertElement(lst, element, pos):
+    """ Inserta el elemento element en la posición pos de la lista.
 
-
-
-def insertElement (lst, element, pos):
-    """ Inserta el elemento element en la posición pos de la lista. 
-    
-    Inserta el elemento en la posición pos de la lista. La lista puede ser vacía.  Se incrementa en 1 el tamaño de la lista.
+    Inserta el elemento en la posición pos de la lista.
+    La lista puede ser vacía.  Se incrementa en 1 el tamaño de la lista.
 
     Args:
         lst: La lista en la que se va a insertar el elemento
         element: El elemento a insertar
-        pos: posición en la que se va a insertar el elemento,  0 < pos <= size(lst) 
+        pos: posición en la que se va a insertar el elemento,
+        0 < pos <= size(lst)
 
     Raises:
         Exception
     """
     try:
-        new_node = node.newSingleNode (element)
+        new_node = node.newSingleNode(element)
         if (pos == 1):
             new_node['next'] = lst['first']
             lst['first'] = new_node
         else:
             cont = 1
             prev = lst['first']
-            current  = lst['first']
+            current = lst['first']
             while cont < pos:
                 prev = current
                 current = current['next']
@@ -333,17 +324,17 @@ def insertElement (lst, element, pos):
         lst['size'] += 1
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->insertElement: ')
+        error.reraise(exp, 'singlelinkedlist->insertElement: ')
 
 
+def isPresent(lst, element):
+    """ Informa si el elemento element esta presente en la lista.
 
-
-def isPresent (lst, element):
-    """ Informa si el elemento element esta presente en la lista. 
-    
-    Informa si un elemento está en la lista.  Si esta presente, retorna la posición en la que se encuentra 
-    o cero (0) si no esta presente. Se utiliza la función de comparación utilizada durante la creación 
-    de la lista para comparar los elementos. La cual debe retornar cero en caso de que los elementos sean iguales.
+    Informa si un elemento está en la lista.  Si esta presente,
+    retorna la posición en la que se encuentra  o cero (0) si no esta presente.
+    Se utiliza la función de comparación utilizada durante la creación
+    de la lista para comparar los elementos.
+    La cual debe retornar cero en caso de que los elementos sean iguales.
 
     Args:
         lst: La lista a examinar
@@ -353,38 +344,37 @@ def isPresent (lst, element):
         Exception
     """
     try:
-        size = lst ['size']
+        size = lst['size']
         if size > 0:
             node = lst['first']
             keyexist = False
-            for keypos in range (1,size+1):
-                if (lst['cmpfunction'] (element, node['info'] )==0):
+            for keypos in range(1, size+1):
+                if (lst['cmpfunction'](element, node['info']) == 0):
                     keyexist = True
                     break
                 node = node['next']
             if keyexist:
                 return keypos
-        return 0        
+        return 0
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->isPresent: ')
+        error.reraise(exp, 'singlelinkedlist->isPresent: ')
 
 
+def changeInfo(lst, pos, newinfo):
+    """ Cambia la informacion contenida en el nodo de la lista que se encuentra
+         en la posicion pos.
 
-
-
-def changeInfo (lst, pos, newinfo):
-    """ Cambia la informacion contenida en el nodo de la lista que se encuentra en la posicion pos.
-    
-    Args:   
+    Args:
         lst: La lista a examinar
         pos: la posición de la lista con la información a cambiar
-        newinfo: La nueva información que se debe poner en el nodo de la posición pos
+        newinfo: La nueva información que se debe poner en el nodo de
+        la posición pos
 
     Raises:
         Exception
     """
     try:
-        current  = lst['first']
+        current = lst['first']
         cont = 1
         while cont < pos:
             current = current['next']
@@ -392,12 +382,10 @@ def changeInfo (lst, pos, newinfo):
         current['info'] = newinfo
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->changeInfo: ')
+        error.reraise(exp, 'singlelinkedlist->changeInfo: ')
 
 
-    
-
-def exchange (lst, pos1, pos2):
+def exchange(lst, pos1, pos2):
     """ Intercambia la informacion en las posiciones pos1 y pos2 de la lista.
 
     Args:
@@ -409,21 +397,20 @@ def exchange (lst, pos1, pos2):
         Exception
     """
     try:
-        infopos1 = getElement (lst, pos1)
-        infopos2 = getElement (lst, pos2)
-        changeInfo (lst, pos1, infopos2)
-        changeInfo (lst, pos2, infopos1)
+        infopos1 = getElement(lst, pos1)
+        infopos2 = getElement(lst, pos2)
+        changeInfo(lst, pos1, infopos2)
+        changeInfo(lst, pos2, infopos1)
         return lst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->exchange: ')
+        error.reraise(exp, 'singlelinkedlist->exchange: ')
 
 
-
-
-def subList (lst, pos, numelem):
+def subList(lst, pos, numelem):
     """ Retorna una sublista de la lista lst.
-    
-    Se retorna una lista que contiene los elementos a partir de la posicion pos, con una longitud de numelem elementos.  
+
+    Se retorna una lista que contiene los elementos a partir de la
+    posicion pos,con una longitud de numelem elementos.
     Se crea una copia de dichos elementos y se retorna una lista nueva.
 
     Args:
@@ -435,15 +422,18 @@ def subList (lst, pos, numelem):
         Exception
     """
     try:
-        sublst = {'first':None, 'last':None, 'size':0, 'type':'SINGLE_LINKED', 'cmpfunction':lst['cmpfunction']}
+        sublst = {'first': None,
+                  'last': None,
+                  'size': 0,
+                  'type': 'SINGLE_LINKED',
+                  'cmpfunction': lst['cmpfunction']}
         cont = 1
         loc = pos
-        while  cont <= numelem:
-            elem = getElement (lst, loc)
-            addLast (sublst, elem)
+        while cont <= numelem:
+            elem = getElement(lst, loc)
+            addLast(sublst, elem)
             loc += 1
             cont += 1
         return sublst
     except Exception as exp:
-        error.reraise (exp, 'singlelinkedlist->subList: ')
-
+        error.reraise(exp, 'singlelinkedlist->subList: ')
---
+++ Test/map/test_map_chaining.py
@@ -0,0 +1,106 @@
+
+import pytest
+import config
+from DISClib.DataStructures import mapentry as me
+from DISClib.ADT import map as ht
+from DISClib.ADT import list as lt
+assert config
+
+
+def compare_keys(key, element):
+    if (int(key) == int(me.getKey(element))):
+        return 0
+    elif (int(key) > int(me.getKey(element))):
+        return 1
+    else:
+        return -1
+
+
+@pytest.fixture
+def map():
+    capacity = 5
+    map = ht.newMap(capacity, maptype='CHAINING', comparefunction=compare_keys)
+
+    ht.put(map, '1', 'title 1')
+    ht.put(map, '2', 'title 2')
+    ht.put(map, '3', 'title 3')
+    ht.put(map, '4', 'title 4')
+    ht.put(map, '5', 'title 5')
+    ht.put(map, '6', 'title 6')
+    ht.put(map, '7', 'title 7')
+    ht.put(map, '8', 'title 8')
+    ht.put(map, '9', 'title 9')
+    ht.put(map, '10', 'title 10')
+    ht.put(map, '11', 'title 11')
+    ht.put(map, '12', 'title 12')
+
+    return map
+
+
+def test_contains(map):
+    assert ht.contains(map, '1') is True
+    assert ht.contains(map, '15') is False
+    assert ht.contains(map, '11') is True
+    assert ht.contains(map, '50') is False
+    assert ht.contains(map, '15') is False
+    assert ht.contains(map, '12') is True
+
+
+def test_get(map):
+    entry = ht.get(map, '5')
+    assert me.getKey(entry) == '5'
+    me.getValue(entry)
+    assert me.getValue(entry) == 'title 5'
+    entry = ht.get(map, '110')
+    assert entry is None
+    ht.put(map, '110', 'title 110')
+    ht.put(map, '120', 'title 120')
+    entry = ht.get(map, '110')
+    assert entry is not None
+    me.getValue(entry)
+    assert me.getValue(entry) == 'title 110'
+
+
+def test_remove(map):
+    assert ht.size(map) == 12
+    assert ht.contains(map, '3') is True
+    ht.remove(map, '3')
+    assert ht.size(map) == 11
+    assert ht.contains(map, '3') is False
+    entry = ht.get(map, '110')
+    assert entry is None
+    ht.put(map, '110', 'title 110')
+    ht.put(map, '111', 'title 111')
+    ht.put(map, '112', 'title 112')
+    ht.put(map, '113', 'title 113')
+    ht.put(map, '114', 'title 114')
+    ht.put(map, '115', 'title 115')
+    assert ht.size(map) == 17
+    ht.put(map, '110', 'title 110-new')
+    ht.put(map, '111', 'title 111-new')
+    ht.put(map, '112', 'title 112-new')
+    ht.put(map, '113', 'title 113-new')
+    ht.put(map, '114', 'title 114-new')
+    ht.put(map, '115', 'title 115-new')
+    assert ht.size(map) == 17
+    entry = ht.get(map, '110')
+    assert me.getValue(entry) == 'title 110-new'
+    entry = ht.remove(map, '110')
+    entry = ht.remove(map, '111')
+    entry = ht.remove(map, '112')
+    entry = ht.remove(map, '113')
+    entry = ht.remove(map, '114')
+    entry = ht.remove(map, '115')
+    assert ht.size(map) == 11
+
+
+def test_getkeys(map):
+    ltset = ht.keySet(map)
+    assert lt.size(ltset) == 12
+    element = lt.getElement(ltset, 1)
+    assert element is not None
+
+
+def test_getvalues(map):
+    ltset = ht.valueSet(map)
+    assert lt.size(ltset) == 12
