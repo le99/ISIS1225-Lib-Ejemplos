18a39f28668df5baee49949d0ad4faf434310970
le99 <le.florez602@uniandes.edu.co>
2022-02-09 16:21:31 -0500

    Ejemplos Listas

---
+++ Ejemplos/listas.py
@@ -0,0 +1,69 @@
+import config as cf
+from DISClib.ADT import list as lt
+
+
+a = lt.newList('SINGLE_LINKED')   #Creacion de una lista vacia con listas sencillamente anlazadas
+a = lt.newList('ARRAY_LIST')      #Creacion de una lista vacia con arreglos
+
+#Agregar Elementos
+lt.addFirst(a, 'a')               # ["a"]
+lt.addFirst(a, 'b')               # ["b", "a"]
+lt.addLast(a, 'c')                # ["b", "a", "c"]
+lt.insertElement(a, "z", 2)       # ["b", "z", "a", "c"]
+lt.insertElement(a, "x", 3)       # ["b", "z", "x", "a", "c"]
+lt.size(a)                        # => 5
+
+#Consultar
+lt.firstElement(a)                # => "b"
+lt.lastElement(a)                 # => "c"
+lt.getElement(a, 3)               # => "x"
+lt.isEmpty(a)                     # => False                    
+
+#Cambiar un elemento
+lt.changeInfo(a, 1, "w")          #["w", "z", "x", "a", "c"]
+
+#Eliminar un elemento
+lt.removeFirst(a)                 #["z", "x", "a", "c"]
+lt.removeLast(a)                  #["z", "x", "a"]
+lt.deleteElement(a, 2)            #["z", "a"]
+
+
+# Recorrer una lista
+for n in lt.iterator(a):
+  pass
+  
+  
+#=================================
+# Ejemplos Avanzados
+#=================================
+
+def compararElementos(e1, e2):
+    if (e1 == e2):
+        return 0
+    elif (e1 > e2):
+        return 1
+    return -1
+
+
+a = lt.newList('ARRAY_LIST', 
+    cmpfunction=compararElementos) #Creacion de una lista vacia, los elementos se pueden comparar con cmpfunction (para poder usar lt.isPresent)
+
+lt.addLast(a, 'a')                # ["a"]
+lt.addLast(a, 'b')                # ["a", "b"]
+lt.addLast(a, 'c')                # ["a", "b", "c"]
+lt.addLast(a, 'd')                # ["a", "b", "c", "d"]
+
+#Intercambiar dos elementos
+lt.exchange(a, 2, 4)              # ["a", "d", "c", "b"]
+sublista = lt.subList(a, 2, 3)    # => ["d", "c", "b"]
+
+
+lt.isPresent(a, "z")              # => 0, no esta 
+lt.isPresent(a, "a")              # => 1, esta en la posicion 1 
+lt.isPresent(a, "d")              # => 1, esta en la posicion 2
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-#ISIS1225 - Librerias de soporte
+# Ejemplos para el uso de DISCLib - Librerias de soporte
 
-Este proyecto contiene los Tipos abstractos de datos, estructuras de datos y algoritmos requeridos por el curso ISIS1225-Estructuras de Datos y Algoritmos
-
--Lib
-Este directorio contiene todo el código de base que se entrega para el funcionamiento del curso.  Dentro de este directorio encuentran:
-    
-    |-- ADT:  Directorio con los Tipos Abstractos de Datos del curso
-
-    |--DataStructures: Directorio con todas las estructuras de datos y archivos auxiliares para su     correcto funcionamiento
-
-    |--Algorithms: Implementación de los algoritmos vistos en el curso (por ejemplo algoritmos de ordenamiento)
-
-    |-- Utils: Funciones auxiliares para el funcionamiento de los TADs y estructuras de datos
-
--Test
-Este directorio contiene las pruebas realizadas a los TADs, Estructuras de Datos y Algoritmos.
-
+Los eejmplos estan en la carpeta `/Ejemplos`
