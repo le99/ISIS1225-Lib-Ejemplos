a344b290626a47e6895af2fefa73bf7459219d27
le99 <le.florez602@uniandes.edu.co>
2022-03-01 11:04:03 -0500

    aclaracion busqueda rango

--- Ejemplos/busquedaRango.py
+++ Ejemplos/busquedaRango.py
@@ -1,4 +1,12 @@
-# Usando Python
+#------------------------------------
+# PRECAUCION!!!:
+# Este codigo NO usa el TAD list, es decir no es codigo valido para retos
+# Sinembargo, ilustra los pasos que se deberian usar:
+#   1) busqueda binaria del indice del elemento mas a la izquierda dentro del rango (bisect_left)
+#   2) busqueda binaria del indice del elemento mas a la derecha dentro del rango (bisect_right - 1)
+#   3) sublist de los elementos en ese rango
+#------------------------------------
+
 
 from bisect import bisect_left, bisect_right
 
@@ -22,17 +30,19 @@ lista = [
 ]
 
 
+# https://docs.python.org/3/howto/sorting.html
 lista.sort(key=lambda x: x["loud"])
 listaLoud = [v["loud"] for v in lista]
 print(listaLoud)
 
+# https://docs.python.org/3/library/bisect.html
 left = bisect_left(listaLoud, 2)
 right = bisect_right(listaLoud, 2)
 
 print("Elementos entre 2 y 2")
 print(lista[left:right])
 
-
-# https://docs.python.org/3/library/bisect.html
-# https://docs.python.org/3/howto/sorting.html
+#Para implmentar bisect_left y bisect_right mirar:
 # https://www.geeksforgeeks.org/find-the-number-of-elements-greater-than-k-in-a-sorted-array/?ref=lbp
+
+
